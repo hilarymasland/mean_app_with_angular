7768f5393915d87e3a4a3eae510cc4b0718d818f
Hilary Masland <hilarymasland@gmail.com>
2016-04-13 19:49:41 -0400

    final angular whenpresident

--- index.js
+++ index.js
@@ -44,10 +44,6 @@ app.use(function(req, res, next){
   });
 });
 
-app.get("/", function(req, res){
-  res.render("candidates");
-});
-
 app.get("/login/twitter", function(req, res){
   twitter.getSigninURL(req, res, function(url){
     res.redirect(url);
@@ -60,53 +56,39 @@ app.get("/login/twitter/callback", function(req, res){
   });
 });
 
-app.get("/candidates", function(req, res){
-  Candidate.find({}).then(function(candidates){
-    res.render("candidates-index", {
-      candidates: candidates
+app.get("/logout", function(req, res){
+  req.session.destroy();
+  res.redirect("/");
+});
+app.get("/api/candidates", function(req, res){
+  Candidate.find({}).lean().exec().then(function(candidates){
+    candidates.forEach(function(candidate){
+      candidate.isCUrrentUser = (candidate._id == req.session.candidate_id);
     });
+    res.json(candidates);
   });
 });
 
-app.get("/candidates/:name", function(req, res){
+app.get("/api/candidates/:name", function(req,res){
   Candidate.findOne({name: req.params.name}).then(function(candidate){
-    res.render("candidates-show", {
-      candidate: candidate,
-      isCurrentUser: (candidate._id == req.session.candidate_id)
-    });
+    res.json(candidate);
   });
 });
 
-app.post("/candidates/:name/delete", function(req, res){
+app.delete("/api/candidates/:name", function(req, res){
   Candidate.findOneAndRemove({name: req.params.name}).then(function(){
-    res.redirect("/candidates")
+    res.json({success: true});
   });
 });
 
-app.post("/candidates/:name", function(req, res){
+app.put("/api/candidates/:name", function(req, res){
   Candidate.findOneAndUpdate({name: req.params.name}, req.body.candidate, {new: true}).then(function(candidate){
-    res.redirect("/candidates/" + candidate.name);
+    res.json(candidate);
   });
 });
-
-app.post("/candidates/:name/positions", function(req, res){
-  Candidate.findOne({name: req.params.name}).then(function(candidate){
-    candidate.positions.push(req.body.position);
-    candidate.save().then(function(){
-      res.redirect("/candidates/" + candidate.name);
-    });
-  });
+app.get("/*", function(req, res){
+  res.render("candidates");
 });
-
-app.post("/candidates/:name/positions/:index", function(req, res){
-  Candidate.findOne({name: req.params.name}).then(function(candidate){
-    candidate.positions.splice(req.params.index, 1);
-    candidate.save().then(function(){
-      res.redirect("/candidates/" + candidate.name);
-    });
-  });
-});
-
 app.listen(app.get("port"), function(){
   console.log("It's aliiive!");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,81 @@
 "use strict";
 
 (function(){
-  
+
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    "$urlRouteProvider",
+    Router
+  ])
+  .factory("Candidate", [
+    "$resource",
+    Candidate
+  ])
+  .controller("candIndexCtrl", [
+    "Candidate",
+    candIndexCtrl
+  ])
+  .controller("candShowCtrl", [
+    "Candidate",
+    "$stateParams",
+    candShowCtrl
   ]);
 
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/candidates-welcome.html"
+    })
+    .state("index", {
+      url: "/candidates",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candIndexCtrl",
+      controllerAs: "indexVM"
+    })
+    .state("show", {
+      url: "/candidates/:name",
+      templateUrl: "/assets/html/candidates-show.html",
+      controller: "candShowCtryl",
+      controllerAs: "showVM"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
+
+  function Candidate($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    Candidate.find = function(property, value, callback){
+      Candidate.all.$promise.then(function(){
+        Candidate.all.forEach(function(candidat){
+          if(candidate[property] == value) callback(candidate);
+        });
+      });
+    }
+    return Candidate;
+  }
+  function candIndexCtrl(Candidate){
+    var vm = this;
+    vm.candidates = Candidate.all;
+  }
+  function candShowCtrl(Candidate, $stateParams){
+    var vm = this;
+    Candidate.find("name", $stateParams.name, function(candidate){
+      vm.candidate = candidate;
+    });
+    vm.update = function(){
+      Candidate.update({name: vm.candidate.name}, {candidate: vm.candidate}, function(){
+        console.log("Done!");
+      });
+    }
+  }
 })();
